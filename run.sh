51d3ed9f5bdcae92a09316790b813b8b047c9471
NUOLEI <nuo.lei@nyu.edu>
2024-04-28 02:05:28 -0400

    update again

--- run.sh
+++ run.sh
@@ -4,7 +4,6 @@
 AGENTS=('DQN' 'PPO' 'A2C')
 CNNS=('Baseline' 'AlexNet' 'ResNet' 'VGG' 'Inception')
 SKIP_FRAME_NUMS=(0 2 5 10)
-STACK_FRAME_NUMS=(0 2 5 10)
 RESIZES=(21 42 84 128)
 GRAY_VALUES=('False' 'True')
 TOTAL_TIMESTEPS=(10000 100000 1000000 2000000 5000000 10000000)
@@ -13,17 +12,15 @@ TOTAL_TIMESTEPS=(10000 100000 1000000 2000000 5000000 10000000)
 for agent in "${AGENTS[@]}"; do
     for cnn in "${CNNS[@]}"; do
         for skip_frame_num in "${SKIP_FRAME_NUMS[@]}"; do
-            for stack_frame_num in "${STACK_FRAME_NUMS[@]}"; do
-                for resize in "${RESIZES[@]}"; do
-                    for gray in "${GRAY_VALUES[@]}"; do
-                        for total_timesteps in "${TOTAL_TIMESTEPS[@]}"; do
-                            # Generate unique model save path based on arguments
-                            model_save_path="./model_file/experiment_${agent}_${cnn}_skip${skip_frame_num}_stack${stack_frame_num}_resize${resize}_gray${gray}_timesteps${total_timesteps}.zip"
-                            
-                            echo "Running with arguments:"
-                            echo "--agent $agent --cnn $cnn --skip_frame_num $skip_frame_num --stack_frame_num $stack_frame_num --resize $resize --gray $gray --total_timesteps $total_timesteps"
-                            python main.py --agent "$agent" --cnn "$cnn" --skip_frame_num "$skip_frame_num" --stack_frame_num "$stack_frame_num" --resize "$resize" --gray "$gray" --total_timesteps "$total_timesteps" --model_save_path "$model_save_path"
-                        done
+            for resize in "${RESIZES[@]}"; do
+                for gray in "${GRAY_VALUES[@]}"; do
+                    for total_timesteps in "${TOTAL_TIMESTEPS[@]}"; do
+                        # Generate unique model save path based on arguments
+                        model_save_path="./model_file/experiment_${agent}_${cnn}_skip${skip_frame_num}_stack${stack_frame_num}_resize${resize}_gray${gray}_timesteps${total_timesteps}.zip"
+                        
+                        echo "Running with arguments:"
+                        echo "--agent $agent --cnn $cnn --skip_frame_num $skip_frame_num --stack_frame_num $stack_frame_num --resize $resize --gray $gray --total_timesteps $total_timesteps"
+                        python main.py --agent "$agent" --cnn "$cnn" --skip_frame_num "$skip_frame_num" --stack_frame_num "$stack_frame_num" --resize "$resize" --gray "$gray" --total_timesteps "$total_timesteps" --model_save_path "$model_save_path"
                     done
                 done
             done
